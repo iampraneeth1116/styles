a5bcd811b7ae48db07e098bad7ee27ae21ca5fa6
Praneeth <iampraneeth1116@gmail.com>
2026-05-05 19:40:05 +0530

    fix: make terraform and ecs workflows fully idempotent

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket  = "shopsmart-tfstate-praneeth1116"
+    key     = "state/terraform.tfstate"
+    encrypt = true
+    # Region is passed via GitHub Actions dynamically
+  }
 }
 
 provider "aws" {
